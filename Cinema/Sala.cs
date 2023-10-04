0a2593fd44c073fcd0d9a07f4be7e3298ecd9153
Victor Matheus da Costa Mariano <victormatheus.mariano7@gmail>
2023-10-03 21:47:37 -0300

    Adicionado funções a classe Sala

--- Cinema/Sala.cs
+++ Cinema/Sala.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Cinema
 {
-    internal class Sala
+    public class Sala
     {
         //Atributos
         private string nome { get; set; }
+        private int qtdCadeiras { get; set; }
+
+        public void Popular()
+        {
+            Console.Write("Digite o nome do filme: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite a duração em minutos do filme: ");
+            qtdCadeiras = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Listar()
+        {
+            Console.WriteLine($" Nome: {nome}");
+            Console.WriteLine($" Quantidade de Cadeiras: {qtdCadeiras}");
+            Console.WriteLine("--------------------------");
+
+        }
     }
 }
 
