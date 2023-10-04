b90f69d4c7c4b45c4eeb1a5df0522d656f8ef8d5
Jadina Maria Garcia <jadinagarcia@gmail.com>
2023-10-03 21:55:58 -0300

    Esqueleto das funções MostrarMenu.

--- Cinema/Menu.cs
+++ Cinema/Menu.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Cinema
 {
-    internal class Menu
+    public class Menu
     {
-        public static string MenuPrincipal()
+        private static string MenuPrincipal()
         {
             Console.WriteLine("Menu principal");
             Console.WriteLine("1 - Clientes");
@@ -17,7 +17,7 @@ namespace Cinema
             Console.WriteLine("0 - Sair");
             return Console.ReadLine();
         }
-        public static string SubMenuCliente()
+        private static string SubMenuCliente()
         {
             Console.WriteLine("1 - Visualizar lista de clientes");
             Console.WriteLine("2 - Visualizar lista de pedidos");
@@ -25,7 +25,7 @@ namespace Cinema
             Console.WriteLine("0 - Retornar");
             return Console.ReadLine();
         }
-        public static string SubMenuCRUD()
+        private static string SubMenuCRUD()
         {
             Console.WriteLine("1 - Adicionar");
             Console.WriteLine("2 - Visualizar");
@@ -33,6 +33,83 @@ namespace Cinema
             Console.WriteLine("4 - Excluir");
             Console.WriteLine("0 - Retornar");
             return Console.ReadLine();
+        }
+        public static void MostrarMenuPrincipal()
+        {
+            Console.Clear();
+            switch (MenuPrincipal())
+            {
+                case "1":
+                    MostrarSubMenuCliente();
+                    break;
+                case "2":
+                    MostrarSubMenuCRUD();
+                    break;
+                case "3":
+                    MostrarSubMenuCRUD();
+                    break;
+                case "0":
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu.");
+                    Console.ReadLine();
+                    MostrarMenuPrincipal();
+                    break;
+            }
+        }
+        public static void MostrarSubMenuCliente()
+        {
+            Console.Clear();
+            switch (SubMenuCliente())
+            {
+                case "1":
+                    //TODO: função visualizar lista de cliente
+                    break;
+                case "2":
+                    //TODO: função visualizar lista de pedidos
+                    break;
+                case "3":
+                    //TODO: função visualizar pedidos por clientes
+                    break;
+                case "0":
+                    MostrarMenuPrincipal();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu.");
+                    Console.ReadLine();
+                    MostrarSubMenuCliente();
+                    break;
+            }
+        }
+        public static void MostrarSubMenuCRUD()
+        {
+            Console.Clear();
+            switch (SubMenuCRUD())
+            {
+                case "1":
+                    //TODO: Create
+                    break;
+                case "2":
+                    //TODO: Read
+                    break;
+                case "3":
+                    //TODO: Update
+                    break;
+                case "4":
+                    //TODO: Delete
+                    break;
+                case "0":
+                    MostrarMenuPrincipal();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu.");
+                    Console.ReadLine();
+                    MostrarSubMenuCliente();
+                    break;
+            }
         }
     }
 }
