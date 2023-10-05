8f601f771f68de134c808e4357eb60c8718ce121
Victor Matheus da Costa Mariano <victormatheus.mariano7@gmail>
2023-10-05 20:16:19 -0300

    Adicionado dados iniciais a lista Sala

--- Cinema/Sala.cs
+++ Cinema/Sala.cs
@@ -12,6 +12,17 @@ namespace Cinema
         private string nome { get; set; }
         private int qtdCadeiras { get; set; }
 
+        public Sala(string nome = "", int qtdCadeiras = 0)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                this.nome = nome;
+            }
+            if (qtdCadeiras > 0)
+            {
+                this.qtdCadeiras = qtdCadeiras;
+            }
+        }
         public void Popular()
         {
             Console.Write("Digite o nome da sala: ");
@@ -20,7 +31,7 @@ namespace Cinema
             Console.Write("Digite a quantidade de cadeiras da sala: ");
             qtdCadeiras = Convert.ToInt32(Console.ReadLine());
         }
-
+        
         public void Listar()
         {
             Console.WriteLine($" Nome: {nome}");
