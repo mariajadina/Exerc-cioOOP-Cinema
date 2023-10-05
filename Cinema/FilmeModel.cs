de5011130aa9b802c287935fe0fc17df6dc6df24
Jadina Maria Garcia <jadinagarcia@gmail.com>
2023-10-05 14:58:20 -0300

    Adição de funções na classe FilmeModel

--- Cinema/FilmeModel.cs
+++ Cinema/FilmeModel.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Cinema
 {
-    internal class FilmeModel
+    public class FilmeModel
     {
+        private List<Filme> filmes = new List<Filme>();
+
+        public void CriarLista()
+        {
+            Filme filme = new Filme();
+            filme.Popular();
+            filmes.Add(filme);
+        }
+
+        public void LerLista()
+        {
+            for (int i = 0; i < filmes.Count; i++)
+            {
+                Console.Write(i + 1);
+                filmes[i].Listar();
+            }
+        }
+
+        public void AtualizarLista()
+        {
+            LerLista();
+            Console.WriteLine("Digite o id do item que deseja alterar:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            filmes[id].Popular();
+        }
+
+        public void RemoverItem()
+        {
+            LerLista();
+            Console.WriteLine("Digite o id do item que deseja remover:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            filmes.RemoveAt(id);
+        }
     }
 }
