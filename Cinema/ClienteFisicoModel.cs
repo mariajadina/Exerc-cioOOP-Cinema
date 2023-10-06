57111d976028b7237b1a7d84a2c50f75b9c1758a
Victor Matheus da Costa Mariano <victormatheus.mariano7@gmail>
2023-10-05 21:49:38 -0300

    Adicionada Classe ClienteFisicoVirtual e alterada Classe FisicoVirtual

---
+++ Cinema/ClienteFisicoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    public class ClienteFisicoModel
+    {
+        private List<ClienteFisico> clientesFisicos = new List<ClienteFisico>();
+        public ClienteFisicoModel()
+        {
+            clientesFisicos.Add(new ClienteFisico("PCD"));
+            clientesFisicos.Add(new ClienteFisico("Assinante do NSC Total"));
+            clientesFisicos.Add(new ClienteFisico("Idoso"));
+            clientesFisicos.Add(new ClienteFisico("PCD"));
+            clientesFisicos.Add(new ClienteFisico("Estudante"));
+            clientesFisicos.Add(new ClienteFisico("Estudante"));
+            clientesFisicos.Add(new ClienteFisico("Idoso"));
+            clientesFisicos.Add(new ClienteFisico("Idoso"));
+        }
+    }
+}
