e9c5ae0ea214fc4cc5d12ef2bac762dbe9952697
Victor Matheus da Costa Mariano <victormatheus.mariano7@gmail>
2023-10-06 21:42:48 -0300

    Adicionado função Listar a Classe ClienteVirtualModel

--- Cinema/ClienteVirtualModel.cs
+++ Cinema/ClienteVirtualModel.cs
@@ -15,7 +15,7 @@ namespace Cinema
             clientesVirtuais.Add(new ClienteVirtual("Lêoncio Walrus", "20/09/1865", "694.965.965-29", "(47) 92654-3165", "leonciobolinadegorfe@gmail.com"));
             clientesVirtuais.Add(new ClienteVirtual("Marcos Brás", "20/09/1976", "265.986.564-52", "(47) 93654-356", "marcosflamengo1981@yahoo.com"));
             clientesVirtuais.Add(new ClienteVirtual("Penelope Charmosa", "20/09/2000", "872.365.965-52", "(47) 95642-1212", "penelopevrumo@yahoo.com"));            
-            clientesVirtuais.Add(new ClienteVirtual("Maduh Silva", "20/09/1976", "596.365.965-08", "(47) 98659-3569", "maduhhhhhhhh@yahoo.com"));
+            clientesVirtuais.Add(new ClienteVirtual("Maduh Silva", "20/09/1976", "596.365.965-09", "(47) 98659-3569", "maduhhhhhhhh@yahoo.com"));
             clientesVirtuais.Add(new ClienteVirtual("Karl Marxs", "05/05/1818", "562.562.564-12", "(47) 9654-3541", "killercapitalistsince1818@yahoo.com"));
             clientesVirtuais.Add(new ClienteVirtual("Pedro Souza", "20/09/1976", "384.254.548-86", "(47) 98542-9853", "pedrosouza@yahoo.com"));
             clientesVirtuais.Add(new ClienteVirtual("Marge Simpson", "20/09/1976", "753.124.698-69", "(47) 96451-3423", "margesimpson@yahoo.com"));
